7acab4344484959d2591cfffb5b038844a6954da
S-Florian <str1florian@gmail.com>
2017-03-11 21:54:55 +0100

    EnkodierungsHelfer._X__ hinzugefuegt

--- src/main/java/me/sflorian/chip8/anweisungen/EnkodierungsHelfer.java
+++ src/main/java/me/sflorian/chip8/anweisungen/EnkodierungsHelfer.java
@@ -12,4 +12,8 @@ public class EnkodierungsHelfer {
     public static short _XY_(int i, byte x, byte y, int j) {
         return (short) ((((i & 0xF) << 12) | ((x & 0xF) << 8) | ((y & 0xF) << 4) | (j & 0xF)) & 0xFFFF);
     }
+
+    public static short _X__(int i, byte x, int j) {
+        return (short) ((((i & 0xF) << 12) | ((x & 0xF) << 8) | (j & 0xFF)) & 0xFFFF);
+    }
 }
--- src/test/java/me/sflorian/chip8/test/EnkodierungsTest.java
+++ src/test/java/me/sflorian/chip8/test/EnkodierungsTest.java
@@ -8,8 +8,8 @@ import static org.junit.Assert.*;
 public class EnkodierungsTest {
     @Test
     public void _XNNTest() {
-        short e = EnkodierungsHelfer._XNN(6, (byte)0xA, (byte)0xEE);
-        assertEquals((short)0x6AEE, e);
+        short e = EnkodierungsHelfer._XNN(6, (byte)0xA, (byte)0xEF);
+        assertEquals((short)0x6AEF, e);
     }
 
     @Test
@@ -22,5 +22,11 @@ public class EnkodierungsTest {
     public void _XY_Test() {
         short e = EnkodierungsHelfer._XY_(8, (byte)0x3, (byte)0x7, 2);
         assertEquals((short)0x8372, e);
+    }
+
+    @Test
+    public void _X__Test() {
+        short e = EnkodierungsHelfer._X__(0xF, (byte)0x2, 0x34);
+        assertEquals((short)0xF234, e);
     }
 }
