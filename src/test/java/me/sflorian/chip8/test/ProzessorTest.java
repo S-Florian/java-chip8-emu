e564c47ee1495507a7c031e75075cf592a7b8ec4
S-Florian <str1florian@gmail.com>
2017-03-11 20:50:48 +0100

    Prozessorbezogene Tests

---
+++ src/test/java/me/sflorian/chip8/test/ProzessorTest.java
@@ -0,0 +1,27 @@
+package me.sflorian.chip8.test;
+
+import me.sflorian.chip8.Arbeitsspeicher;
+import me.sflorian.chip8.Prozessor;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class ProzessorTest {
+    @Test
+    public void regVTest() {
+        Prozessor p = new Prozessor(new Arbeitsspeicher());
+
+        for (int i = -2; i < Prozessor.CHIP8_ANZAHL_V_REGISTER + 2; ++i) {
+            p.regVSetzen(i, (byte)i);
+        }
+
+        assertEquals(0, p.regVGeben(-2));
+        assertEquals(0, p.regVGeben(-1));
+
+        for (int i = 0; i < Prozessor.CHIP8_ANZAHL_V_REGISTER; ++i) {
+            assertEquals(i, p.regVGeben(i));
+        }
+
+        assertEquals(0, p.regVGeben(Prozessor.CHIP8_ANZAHL_V_REGISTER + 1));
+    }
+}
